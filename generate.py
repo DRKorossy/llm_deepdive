2fbdb6383f0f487d8051f54a32053d91ca52345e
David Korossy <142813581+DRKorossy@users.noreply.github.com>
2026-07-18 21:10:03 +0100

    Implement Text generation (Build GPT 8/10)

---
+++ generate.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+from torchtyping import TensorType
+
+class Solution:
+    def generate(self, model, new_chars: int, context: TensorType[int], context_length: int, int_to_char: dict) -> str:
+        generator = torch.manual_seed(0)
+        initial_state = generator.get_state()
+
+        ontext = context[:, -context_length:]
+        logits = model(context)
+        last_position_logits = logits[:, -1, :]
+        probs = torch.softmax(last_position_logits, dim=-1)
+        samples = []
+        for i in range(new_chars):
+            sample = torch.multinomial(probs, 1, generator=generator)
+            generator.set_state(initial_state)
+            samples.append(int_to_char[sample.item()])
+        return "".join(samples)
