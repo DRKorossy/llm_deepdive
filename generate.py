fe927a90f7a05a80db6fff845516894381628c76
David Korossy <142813581+DRKorossy@users.noreply.github.com>
2026-07-18 21:22:52 +0100

    Update Text generation (Build GPT 8/10)

--- generate.py
+++ generate.py
@@ -7,14 +7,17 @@ class Solution:
         generator = torch.manual_seed(0)
         initial_state = generator.get_state()
 
-        context = context[:, -context_length:]
-        logits = model(context)
-        last_position_logits = logits[:, -1, :]
-        probs = torch.softmax(last_position_logits, dim=-1)
         
         samples = []
         for i in range(new_chars):
+            if context.shape[1] > context_length:
+                context = context[:, -context_length:]
+            logits = model(context)
+            last_position_logits = logits[:, -1, :]
+            probs = nn.functional.softmax(last_position_logits, dim=-1)
             sample = torch.multinomial(probs, 1, generator=generator)
             generator.set_state(initial_state)
+
+            context = torch.cat([context, sample], dim=-1)
             samples.append(int_to_char[sample.item()])
         return "".join(samples)
